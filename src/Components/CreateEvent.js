51b09f4ae8d53845965a140b0805697fd0ab8cb3
Dhaval saxena <dhaval.saxena@minddeft.net>
2023-05-03 16:32:07 +0530

    #fetchingEvent

--- src/Components/CreateEvent.js
+++ src/Components/CreateEvent.js
@@ -1,40 +1,113 @@
-import React, { useState, useEffect,useContext } from "react";
-import DataContext from '../Context/dataContext';
+import React, { useState, useEffect, useContext } from "react";
+import DataContext from "../Context/dataContext";
 
 export default function CreateEvent() {
-    const data=useContext(DataContext);
-return(
-    <>
-    <center>
-    <h3>CreateEvent here</h3>
-    </center>
-    <div className="container">
-    <form>
-        
-  <div class="form-group">
-    <input type="email" class="form-control" id=" EventId" aria-describedby="emailHelp" placeholder="Enter Event Id"/>
-  </div>
-  <div class="form-group">
-    <input type="email" class="form-control" id="EventName" aria-describedby="emailHelp" placeholder="Enter EventName"/>
-  </div>
-  <div class="form-group">
-    <input type="email" class="form-control" id="Date" aria-describedby="emailHelp" placeholder="Enter Date"/>
-  </div>
-  <div class="form-group">
-    <input type="email" class="form-control" id="startBooking" aria-describedby="emailHelp" placeholder="Enter startBooking"/>
-  </div>
-  <div class="form-group">
-    <input type="email" class="form-control" id="endBooking" aria-describedby="emailHelp" placeholder="Enter endBooking"/>
-  </div>
-  <div class="form-group">
-    <input type="email" class="form-control" id="tickets" aria-describedby="emailHelp" placeholder="Enter tickets"/>
-  </div>
- 
- 
-  <button type="submit" class="btn btn-dark">Create</button>
-</form>
-</div>
-    </>
-)
+  const context = useContext(DataContext);
+  const { addEvent } = context;
 
+  const [event, setEvents] = useState({
+    eventId: " ",
+    EventName: " ",
+    Date: 0,
+    startBooking: 0,
+    endBooking: 0,
+    tickets:0,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEvent(
+      event.eventId,
+      event.EventName,
+      event.Date,
+      event.startBooking,
+      event.endBooking,
+      event.tickets
+    );
+    console.log("Calling handleSubmit.......");
+  };
+
+  const onChange = (e) => {
+    setEvents({ ...event, [e.target.name]: e.target.value });
+  };
+  return (
+    <>
+      <center>
+        <h3>CreateEvent here</h3>
+      </center>
+      <div className="container">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="eventId"
+              name="eventId"
+              onChange={onChange}
+              aria-describedby="emailHelp"
+              placeholder="Enter Event Id"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="EventName"
+              name="EventName"
+              onChange={onChange}
+              aria-describedby="emailHelp"
+              placeholder="Enter EventName"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="Date"
+              name="Date"
+              onChange={onChange}
+              aria-describedby="emailHelp"
+              placeholder="Enter Date"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="startBooking"
+              name="startBooking"
+              onChange={onChange}
+              aria-describedby="emailHelp"
+              placeholder="Enter startBooking"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="endBooking"
+              name="endBooking"
+              onChange={onChange}
+              aria-describedby="emailHelp"
+              placeholder="Enter endBooking"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="tickets"
+              name="tickets"
+              aria-describedby="emailHelp"
+              placeholder="Enter tickets"
+            />
+          </div>
+
+          <button type="submit" onClick={handleSubmit} className="btn btn-dark">
+            Create
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/Components/EventTotal.js
+++ src/Components/EventTotal.js
@@ -1,22 +1,27 @@
 import React, { useState, useEffect, useContext } from "react";
 import ViewEvents from "../Components/ViewEvents";
+import CreateEvent from "../Components/CreateEvent";
 import DataContext from "../Context/dataContext";
 import { Link } from "react-router-dom";
-
-export default function EventTotal({ object }) {
+ 
+export default function EventTotal() {
   const context = useContext(DataContext);
-  const { events, setEvents } = context;
-
+  const { events, setEvents, addNote , getData } = context;
+  useEffect(() => {
+    getData()
+  },[])
   return (
     <>
+    {/* <CreateEvent/> */}
       {/* <h4> {event.EventName}</h4>;   */}
          {events.map((event) => { 
-           return <div class="card-group mt-3">
-           <div class="card">
-             <div class="card-body">
-               <h5 class="card-title">{event.EventName}</h5>
-               <p class="card-text">  Booking starts {event.Date}.</p>
-               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+           return <div className="card-group mt-3">
+           <div  className="card">
+             <div  className="card-body">
+            <i>#{event.eventId}</i>
+               <h5  className="card-title">{event.EventName}</h5>
+               <p  className="card-text">  Booking starts {event.Date}.</p>
+               <p  className="card-text"><small  className="text-muted">EventTotal {events.length}</small></p>
                <button className="btn btn-outline-success">Book</button>
              </div>
            </div>
@@ -24,15 +29,15 @@ export default function EventTotal({ object }) {
 
 
            
-        //    <div class="card"style={{ width: '18rem' }}>
-        //   <div class="card-body">
+        //    <div  className="card"style={{ width: '18rem' }}>
+        //   <div  className="card-body">
         //     <i>{event.eventId}</i>
-        //     <h6 class="card-title">{event.EventName}</h6>
-        //     <p class="card-text">
+        //     <h6  className="card-title">{event.EventName}</h6>
+        //     <p  className="card-text">
         //     Booking starts {event.Date}
         //     Date starts {event.startBooking}
         //     </p>
-        //     <Link to="#" class="btn btn-primary">
+        //     <Link to="#"  className="btn btn-primary">
         //      Book
         //     </Link>
         //   </div>
